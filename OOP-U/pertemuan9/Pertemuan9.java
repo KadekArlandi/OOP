881d3247c7597a02b8377c00ffcfe75ec1ce11d0
KadekArlandi <127810806+KadekArlandi@users.noreply.github.com>
2024-05-13 12:16:13 +0800

    Add files via upload

---
+++ OOP-U/pertemuan9/Pertemuan9.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author KADEK ARLANDI MAHESA GAUTAMA PUTRA
+ * 2201010241
+ * 13-05-2024
+ */
+public class Pertemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //new frm01().setVisible(true);
+        
+        frm01 f1 = new frm01();
+        f1.setResizable(true);
+        f1.setAlwaysOnTop(true);
+    }
+    
+}
